702c59491874b1b0c56678590a751bbf338e3115
ShubhamKRay <shubhairmen@gmail.com>
2026-03-26 20:32:50 +0530

    ClassPathXmlApplicationContext

---
+++ Spring-2/src/com/mainapp/Launch.java
@@ -0,0 +1,15 @@
+package com.mainapp;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class Launch {
+
+	public static void main(String[] args) {
+
+		ClassPathXmlApplicationContext ac = new ClassPathXmlApplicationContext("bean.xml");
+
+		Employee emp = (Employee) ac.getBean("emp");
+		emp.test();
+	}
+
+}
